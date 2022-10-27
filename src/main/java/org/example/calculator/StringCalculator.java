61faaafca3c0ec5b1f0d1d3b0819e7fa0c7f9519
Leung Woon Hay <leungwoonhay@Ngas-Air.mynet>
2022-10-27 18:28:37 +0100

    Step1 complete

--- src/main/java/org/example/calculator/StringCalculator.java
+++ src/main/java/org/example/calculator/StringCalculator.java
@@ -7,7 +7,16 @@ public class StringCalculator {
     private static Logger stringCalculatorLogger = Logger.getLogger("stringCalculatorLogger");
 
     public int Add(String numbers) {
+        int result = 0;
+        numbers = numbers.replaceAll("\\s", "");
         stringCalculatorLogger.log(Level.INFO, "Method start");
-        return 0;
+        if (numbers.equals(""))
+            result = 0;
+        else {
+            String[] strings = numbers.split(",");
+            for (int i = 0; i < strings.length; i ++)
+                result += Integer.valueOf(strings[i]);
+        }
+        return result;
     }
 }
