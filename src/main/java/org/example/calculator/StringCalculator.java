ea9c74abf21e6d71a9cfad22b32f492d05a4dd62
Leung Woon Hay <leungwoonhay@Ngas-Air.mynet>
2022-10-28 09:29:00 +0100

    Step5 complete

--- src/main/java/org/example/calculator/StringCalculator.java
+++ src/main/java/org/example/calculator/StringCalculator.java
@@ -1,12 +1,16 @@
 package org.example.calculator;
 
+import org.example.exception.NegativeNotAllowedException;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class StringCalculator {
     private static Logger stringCalculatorLogger = Logger.getLogger("stringCalculatorLogger");
 
-    public int Add(String numbers) {
+    public int Add(String numbers) throws NegativeNotAllowedException {
         int result = 0;
         stringCalculatorLogger.log(Level.INFO, "Method start");
         if (numbers.equals(""))
@@ -23,11 +27,26 @@ public class StringCalculator {
         return result;
     }
 
-    public int addNumbers(String numbers, String delimiter) {
+    public int addNumbers(String numbers, String delimiter) throws NegativeNotAllowedException {
         int result = 0;
+        List<String> negativeNumber = new ArrayList<>();
+
         String[] strings = numbers.split(delimiter);
         for (int i = 0; i < strings.length; i ++)
-            result += Integer.valueOf(strings[i]);
+            if (Integer.valueOf(strings[i]) < 0)
+                negativeNumber.add(strings[i]);
+            else
+                result += Integer.valueOf(strings[i]);
+
+        if (negativeNumber.size() > 0) {
+            String message = "Negatives not allowed: ";
+            for (int i = 0; i < negativeNumber.size(); i ++)
+                message += negativeNumber.get(i) + ",";
+            message = message.substring(0, message.length() - 1);
+
+            throw new NegativeNotAllowedException(message);
+        }
+
         return result;
     }
 }
--- src/test/java/org/example/CalculateTester.java
+++ src/test/java/org/example/CalculateTester.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.calculator.StringCalculator;
+import org.example.exception.NegativeNotAllowedException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,38 +15,50 @@ public class CalculateTester {
     }
 
     @Test
-    public void testEmptyString() {
+    public void testEmptyString() throws NegativeNotAllowedException {
         int result = stringCalculator.Add("");
         Assertions.assertEquals(0, result);
     }
 
     @Test
-    public void testOneNumber() {
+    public void testOneNumber() throws NegativeNotAllowedException {
         int result = stringCalculator.Add("1");
         Assertions.assertEquals(1, result);
     }
 
     @Test
-    public void testTwoNumber() {
+    public void testTwoNumber() throws NegativeNotAllowedException {
         int result = stringCalculator.Add("1,2");
         Assertions.assertEquals(3, result);
     }
 
     @Test
-    public void testUnknownNumberOfNumbers() {
+    public void testUnknownNumberOfNumbers() throws NegativeNotAllowedException {
         int result = stringCalculator.Add("1,2,3,4,5");
         Assertions.assertEquals(15, result);
     }
 
     @Test
-    public void testNewLines() {
+    public void testNewLines() throws NegativeNotAllowedException {
         int result = stringCalculator.Add("1\n2,3");
         Assertions.assertEquals(6, result);
     }
 
     @Test
-    public void testDifferentDelimiter() {
+    public void testDifferentDelimiter() throws NegativeNotAllowedException {
         int result = stringCalculator.Add("//;\n1;2");
         Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    public void testNegativeNumber() {
+        NegativeNotAllowedException exception = Assertions.assertThrows(NegativeNotAllowedException.class, () -> stringCalculator.Add("-1,2"));
+        Assertions.assertEquals("Negatives not allowed: -1", exception.getMessage());
+    }
+
+    @Test
+    public void testNegativeNumber2() {
+        NegativeNotAllowedException exception = Assertions.assertThrows(NegativeNotAllowedException.class, () -> stringCalculator.Add("2,-4,3,-5"));
+        Assertions.assertEquals("Negatives not allowed: -4,-5", exception.getMessage());
     }
 }
